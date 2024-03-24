dd6d32a0e3d6579849ebadb6617fec1f0199f294
ekarpp <eetu@karppinen.xyz>
2024-03-24 16:58:02 +0200

    fix microfacet transmission and add fresnel importance sampling to it

--- src/tracer/bxdf/microfacet.rs
+++ src/tracer/bxdf/microfacet.rs
@@ -4,8 +4,14 @@ use super::*;
 mod util {
     use super::*;
 
-    pub fn reflect(v: Direction, no: Normal) -> Direction {
-        2.0 * v.project_onto(no) - v
+    pub fn reflect(v: Direction, no: Normal) -> Option<Direction> {
+        let wi = 2.0 * v.project_onto(no) - v;
+        if wi.z <= 0.0 {
+            // bad sample, do something else?
+            None
+        } else {
+            Some( wi )
+        }
     }
 
     pub fn refract(eta_ratio: Float, v: Direction, no: Normal) -> Option<Direction> {
@@ -69,14 +75,7 @@ pub fn reflection_sample(
 ) -> Option<Direction> {
     let v = -wo;
     let wh = mfd.sample_normal(v, rand_sq).normalize();
-    let wi = util::reflect(v, wh);
-
-    if wi.z <= 0.0 {
-        // bad sample, do something else?
-        None
-    } else {
-        Some( wi )
-    }
+    util::reflect(v, wh)
 }
 
 pub fn reflection_pdf(
@@ -140,10 +139,7 @@ pub fn transmission_f(
     } else {
         mfd.eta()
     };
-    let scale = match mode {
-        Transport::Radiance => eta_ratio * eta_ratio,
-        Transport::Importance => 1.0,
-    };
+
     let wh = (wi * eta_ratio + v).normalize();
     let wh = if wh.z < 0.0 { -wh } else { wh };
 
@@ -158,11 +154,16 @@ pub fn transmission_f(
     let d = mfd.d(wh);
     let f = mfd.f(v, wh);
     let g = mfd.g(v, wi, wh);
+    let scale = match mode {
+        Transport::Radiance => eta_ratio * eta_ratio,
+        Transport::Importance => 1.0,
+    };
 
-    albedo * (scale * d * (1.0 - f) * g
+    let transmission = scale * d * (1.0 - f) * g
         * (wh_dot_wi * wh_dot_v / (cos_theta_wi * cos_theta_v)).abs()
-        / (eta_ratio * wh_dot_wi + wh_dot_v).powi(2)
-        + util::reflect_coeff(wo, wi, mfd))
+        / (eta_ratio * wh_dot_wi + wh_dot_v).powi(2);
+    let reflection = util::reflect_coeff(wo, wi, mfd);
+    albedo * (transmission + reflection)
 }
 
 pub fn transmission_sample(
@@ -179,14 +180,21 @@ pub fn transmission_sample(
         mfd.sample_normal(v, rand_sq).normalize()
     };
 
+    // importance sample reflection/transmission with fresnel
+    let pr = mfd.f(v, wh);
+    let pt = 1.0 - pr;
 
-    let eta_ratio = if inside {
-        mfd.eta()
+    if rand_utils::rand_float() < pr / (pr + pt) {
+        util::reflect(v, wh)
     } else {
-        1.0 / mfd.eta()
-    };
+        let eta_ratio = if inside {
+            mfd.eta()
+        } else {
+            1.0 / mfd.eta()
+        };
 
-    util::refract(eta_ratio, v, wh)
+        util::refract(eta_ratio, v, wh)
+    }
 }
 
 pub fn transmission_pdf(
@@ -199,29 +207,32 @@ pub fn transmission_pdf(
     let (v, wi) = if swap_dir { (wi, v) } else { (v, wi) };
     let v_inside = v.z < 0.0;
     let wi_inside = wi.z < 0.0;
+    let is_reflection = v_inside == wi_inside;
 
-    if v_inside == wi_inside {
-        /* same hemisphere */
-        0.0
+    let eta_ratio = if is_reflection {
+        1.0
     } else {
-        let eta_ratio = if v_inside {
-            1.0 / mfd.eta()
-        } else {
-            mfd.eta()
-        };
+        if v_inside { 1.0 / mfd.eta() } else { mfd.eta() }
+    };
 
-        let wh = -(v + wi * eta_ratio).normalize();
-        let wh_dot_wi = wi.dot(wh);
-        let wh_dot_v = wh.dot(v);
+    let wh = (v + wi * eta_ratio).normalize();
+    let wh = if wh.z < 0.0 { -wh } else { wh };
+    let wh_dot_v = v.dot(wh);
+    let wh_dot_wi = wi.dot(wh);
 
-        if wh_dot_wi * wh_dot_v > 0.0 {
-            /* same hemisphere w.r.t. wh */
-            0.0
-        } else {
-            let wh = if wh.z < 0.0 { -wh } else { wh };
-            mfd.sample_normal_pdf(wh, v)
-                * (eta_ratio * eta_ratio * wh_dot_wi).abs()
-                / (wh_dot_v + eta_ratio * wh_dot_wi).powi(2)
-        }
+    // discard backfacing wh
+    if wh_dot_v * v.z < 0.0 || wh_dot_wi * wi.z < 0.0 {
+        return 0.0;
+    }
+
+    let pr = mfd.f(v, wh);
+    let pt = 1.0 - pr;
+
+    if is_reflection {
+        mfd.sample_normal_pdf(wh, v) / (4.0 * wh_dot_v.abs())
+            * pr / (pr + pt)
+    } else {
+        mfd.sample_normal_pdf(wh, v) * wh_dot_wi.abs() / (wh_dot_wi + wh_dot_v / eta_ratio).powi(2)
+            * pt / (pr + pt)
     }
 }
